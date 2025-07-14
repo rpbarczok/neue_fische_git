611f429d960f6772a6ca885365df6dc73098f0c0
Ralph <rpbarczok@gmx.de>
2025-07-14 09:42:01 +0200

    test commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -35,7 +35,7 @@ public class Main {
 
         num.forEach(System.out::println);
 
-
+    // Das ist ein test comment
 
         System.out.println("--------------------------------------");
         // Bonus-Aufgabe Studenten
